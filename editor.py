83b1f781f9a3f5b004ed6cf1281a796beead6aa4
guldfisk <ce.guldfisk@gmail.com>
2017-12-09 22:34:10 +0100

    damn

--- editor.py
+++ editor.py
@@ -1,18 +1,27 @@
 import os
 import re
 import sys
+import threading
+import time
+import itertools
+import pickle
 
+import typing as t
 import xml.etree.ElementTree as ET
+
+from promise import Promise
 from PIL import ImageQt
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui, Qt
 from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene, QMainWindow, QAction, QGraphicsPixmapItem
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QTransform
 
 from mtgorp.db.load import Loader as MtgLoader
 from mtgorp.db.load import CardDatabase
 from mtgorp.models.persistent import printing as _printing
 
-from mtgimg.load import Loader as ImageLoader
+from orp.relationships import One, Many, OneDescriptor
+
+from mtgimg import load as imageload
 
 
 class Globals(object):
@@ -21,39 +30,270 @@ class Globals(object):
 	def init(cls):
 		cls.db = MtgLoader.load()
 
-class Card(QGraphicsPixmapItem):
+class ImageLoader(imageload.Loader):
+	@classmethod
+	def get_pixmap(
+		cls,
+		printing: _printing.Printing = None,
+		back: bool = False,
+		crop: bool = False,
+		image_request: imageload.ImageRequest = None
+	) -> Promise:
+		return cls.get_image(
+			printing,
+			back,
+			crop,
+			image_request,
+		).then(
+			lambda image: QPixmap.fromImage(
+				ImageQt.ImageQt(
+					image
+				)
+			)
+		)
+	@classmethod
+	def get_default_pixmap(cls):
+		print('get default pixmap')
+		return cls.get_default_image().then(
+			lambda image: QPixmap.fromImage(ImageQt.ImageQt(image))
+		)
+
+class TestThread(threading.Thread):
+	def __init__(self, f, *args, **kwargs):
+		super().__init__()
+		self.f = f
+		self.args = args
+		self.kwargs = kwargs
+	def run(self):
+		time.sleep(3)
+		self.f(*self.args, **self.kwargs)
+
+class Card(QtWidgets.QGraphicsObject):
+	signal = QtCore.pyqtSignal(object)
+	# noinspection PyCallByClass
 	def __init__(self, printing: _printing.Printing):
 		super().__init__()
+		self._selection_highlite_pen = QtGui.QPen(QtGui.QColor(255, 0, 0), 2)
 		self._printing = printing
-		self.setPixmap(
-			QPixmap.fromImage(
-				ImageQt.ImageQt(
-					ImageLoader.get_image(
-						printing,
-						callback=self._set_image,
-						async=False,
-					)
+		self._pixmap = None #type: QtGui.QPixmap
+		self._bounding_rect = QtCore.QRectF()
+		pixmap_promise = ImageLoader.get_pixmap(self._printing)
+		if pixmap_promise.is_fulfilled:
+			self.setPixmap(pixmap_promise.get())
+			print('bounding rect', self.boundingRect().x(), self.boundingRect().y(), self.boundingRect().width(), self.boundingRect().height())
+		else:
+			print('pic not on local')
+			print(isinstance(self, QGraphicsPixmapItem))
+			print(dir(self))
+			self.setPixmap(ImageLoader.get_default_pixmap().get())
+
+			self.signal.connect(self._set_image)
+			print('connected')
+		pixmap_promise.then(self.signal.emit)
+		self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable)
+		self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
+		self.setFlag(QtWidgets.QGraphicsItem.ItemSendsGeometryChanges)
+		# self.move_to_top()
+		self._card_stacker = None #type: CardStacker
+	@property
+	def card_stacker(self):
+		return self._card_stacker
+	@card_stacker.setter
+	def card_stacker(self, card_stacker: 'CardStacker'):
+		self._card_stacker = card_stacker
+	def pixmap(self):
+		return self._pixmap
+	def setPixmap(self, pixmap: 'QtGui.QPixmap'):
+		self._pixmap = pixmap
+		self._bounding_rect = QtCore.QRectF(0, 0, pixmap.size().width(), pixmap.size().height())
+	def paint(self, painter: QtGui.QPainter, options, widget=None):
+		painter.drawPixmap(QtCore.QPointF(0, 0), self._pixmap)
+		if self.isSelected():
+			painter.setPen(self._selection_highlite_pen)
+			painter.drawRect(self.boundingRect())
+	def boundingRect(self):
+		return self._bounding_rect
+	def _set_image(self, pixmap: QPixmap):
+		print('set image')
+		# pixmap = ImageLoader.get_default_pixmap().get()
+		print('setting real pixmap', pixmap, type(pixmap), pixmap.size())
+		self.setPixmap(pixmap)
+		print('done, updating')
+		self.update()
+		try:
+			print(
+				'update done',
+				self.pixmap().size(),
+				(self.pos().x(), self.pos().y()), (self.scenePos().x(), self.scenePos().y()),
+				self.boundingRect(),
+			)
+		except Exception as e:
+			print('rip', e)
+	def align(self, pos: QtCore.QPointF):
+		self.scene().get_card_stacker(pos.x(), pos.y()).add_card(self)
+	def mouseDoubleClickEvent(self, *args, **kwargs):
+		print('double clicked')
+		ImageLoader.get_pixmap(Globals.db.cardboards['Lightning Bolt'].from_expansion('LEA')).then(self._set_image)
+	def mouseReleaseEvent(self, event):
+		super().mouseReleaseEvent(event)
+		print('mouse release event', self, event.pos(), self.scene())
+		print(self.scene().get_card_stacker(event.scenePos().x(), event.scenePos().y()))
+		for item in self.scene().selectedItems():
+			if isinstance(item, Card):
+				item.align(event.scenePos())
+	def mousePressEvent(self, event):
+		super().mousePressEvent(event)
+		print('mouse pressed', self, event, self.scene().selectedItems())
+		for item in self.scene().selectedItems():
+			print(item.card_stacker)
+			if isinstance(item, Card) and item.card_stacker is not None:
+				print('lets go', item.card_stacker.cards)
+				print(item)
+				item.card_stacker.remove_card(item)
+				print('card removed')
+	def itemChange(self, change, value):
+		# super().itemChange(change, value)
+		# if change==QtWidgets.QGraphicsItem.ItemSelectedChange and value:
+		# 	self.move_to_top()
+		if change==QtWidgets.QGraphicsItem.ItemSceneChange:
+			if self._card_stacker is not None:
+				self._card_stacker.remove_card(self)
+		# if change==QtWidgets.QGraphicsItem.ItemPositionHasChanged:
+		# 	print('position change', value)
+		# return QtWidgets.QGraphicsItem.itemChange(change, value)
+		return value
+
+class CardStacker(object):
+	def __init__(self, position: t.Tuple[float, float], spacing = 100):
+		self._position = position
+		self._spacing = spacing
+		self.cards = [] #type: t.List[Card]
+		self._z = 0
+	def stack(self):
+		for i in range(len(self.cards)):
+			self.cards[i].setPos(
+				QtCore.QPoint(
+					self._position[0],
+					self._position[1] + i*self._spacing
 				)
 			)
-		)
-	def _set_image(self, image_request, result):
-		self.setPixmap(
-			QPixmap.fromImage(
-				ImageQt.ImageQt(
-					result
-				)
+			self.cards[i].setZValue(self._z+i)
+	def _remove_card(self, card: Card):
+		try:
+			self.cards.remove(card)
+			card.card_stacker = None
+		except KeyError:
+			pass
+	def _add_card(self, card: Card):
+		if card.card_stacker is not None:
+			card.card_stacker.remove_card(card)
+		card.card_stacker = self
+		self.cards.append(card)
+	def remove_card(self, card: Card):
+		self._remove_card(card)
+		self.stack()
+	def add_card(self, card: Card):
+		self._add_card(card)
+		self.stack()
+	def remove_cards(self, cards: t.Iterable[Card]):
+		for card in cards:
+			self._remove_card(card)
+		self.stack()
+	def add_cards(self, cards: t.Iterable[Card]):
+		for card in cards:
+			self._add_card(card)
+		self.stack()
+
+class CardScene(QGraphicsScene):
+	def __init__(self):
+		super().__init__()
+		self._card_stackers = {} #type: t.Dict[t.Tuple[int, int], CardStacker]
+		self._card_stacker_width = 1000
+		self._card_stacker_height = 1400
+	def get_card_stacker(self, x, y):
+		key = x // self._card_stacker_width, y // self._card_stacker_height
+		try:
+			return self._card_stackers[key]
+		except KeyError:
+			self._card_stackers[key] = CardStacker(
+				(key[0]*self._card_stacker_width,
+				 key[1]*self._card_stacker_height)
 			)
-		)
+			return self._card_stackers[key]
 
 class CardContainer(QGraphicsView):
 	def __init__(self):
-		self._graphic_scene = QGraphicsScene()
+		self._graphic_scene = CardScene()
 		super().__init__(self._graphic_scene)
+		self.setDragMode(QGraphicsView.RubberBandDrag)
+		self.setAcceptDrops(True)
+
 		printing = Globals.db.cardboards['Bloodghast'].from_expansion('ZEN')
+		another_printing = Globals.db.cardboards['Dive Down'].from_expansion('XLN')
+		card = Card(printing)
 		self._graphic_scene.addItem(
-			Card(printing)
+			card
 		)
-		self._graphic_scene.addText(printing.cardboard.name)
+		card.moveBy(40, 40)
+		self._graphic_scene.addItem(Card(another_printing))
+	def dragEnterEvent(self, drag_event: QtGui.QDragEnterEvent):
+		if drag_event.source() is not None and isinstance(drag_event.source(), CardContainer):
+			drag_event.accept()
+	def dragMoveEvent(self, drag_event: QtGui.QDragMoveEvent):
+		pass
+	def dropEvent(self, drop_event: QtGui.QDropEvent):
+		if drop_event.source()==self:
+			for item in self.scene().selectedItems():
+				item.align(self.mapToScene(drop_event.pos()))
+		else:
+			for item in drop_event.source().scene().selectedItems():
+				self.scene().addItem(item)
+				item.align(self.mapToScene(drop_event.pos()))
+	def mouseMoveEvent(self, mouse_event: QtGui.QMouseEvent):
+		super().mouseMoveEvent(mouse_event)
+		if not QtCore.QRectF(0, 0, self.size().width(), self.size().height()).contains(mouse_event.pos()):
+			# mouse_event.accept()
+			drag = QtGui.QDrag(self)
+			# mime = QtCore.QMimeData()
+			# stream = QtCore.QByteArray()
+			# stream.append(pickle.dumps((1, 2, 3)))
+			# mime.setData('cards', stream)
+			# drag.setMimeData(mime)
+			# print('drag', drag, mime)
+			drag.exec_()
+		# 	return
+		# mouse_event.ignore()
+	# def mousePressEvent(self, mouse_event: QtGui.QMouseEvent):
+	# 	super().mousePressEvent(mouse_event)
+	# 	print(
+	# 		'mouse press event',
+	# 		self.itemAt(mouse_event.pos()),
+	# 		mouse_event.pos(),
+	# 	)
+	# def mouseMoveEvent(self, mouse_event: QtGui.QMouseEvent):
+	# 	super().mouseMoveEvent(mouse_event)
+	# 	# print('mouse move event', mouse_event, self)
+
+class ScaleSlider(QtWidgets.QSlider):
+	def __init__(self, *__args):
+		super().__init__(QtCore.Qt.Horizontal)
+		self.setMinimum(1)
+		self.setMaximum(100)
+		self.setValue(100)
+
+class CardWindow(QtWidgets.QWidget):
+	def __init__(self):
+		super().__init__()
+		self.slider = ScaleSlider(Qt)
+		self.card_container = CardContainer()
+		self.slider.valueChanged.connect(
+			lambda v: self.card_container.setTransform(QTransform().scale(v/100, v/100))
+		)
+		self.slider.setValue(50)
+		box = QtWidgets.QVBoxLayout(self)
+		box.addWidget(self.card_container)
+		box.addWidget(self.slider)
+		self.setLayout(box)
 
 class MainView(QWidget):
 	def __init__(self, parent=None):
@@ -83,9 +323,14 @@ class MainView(QWidget):
 		# box.addWidget(topsplitter)
 		# box.addLayout(vbox)
 
-		self.card_container = CardContainer()
+		# self.card_window = CardWindow()
 
-		box.addWidget(self.card_container)
+		box.addWidget(
+			CardWindow()
+		)
+		box.addWidget(
+			CardWindow()
+		)
 
 		self.setLayout(
 			box
@@ -96,15 +341,15 @@ class MainWindow(QMainWindow):
 		super(MainWindow,self).__init__(parent)
 
 		# self.setWindowIcon(QtGui.QIcon(os.path.join(locate.path, 'handelsforbud.png')))
-		
+
 		self.mainview = MainView()
-		
+
 		self.setCentralWidget(self.mainview)
-		
+
 		self.setWindowTitle('Deckeditor')
-		
+
 		menubar = self.menuBar()
-		
+
 		allMenues = {
 			menubar.addMenu('File'): (
 				('Exit', 'Ctrl+Q', QtWidgets.qApp.quit),
@@ -121,15 +366,18 @@ class MainWindow(QMainWindow):
 				('Add cards', 'Ctrl+f', self.addCard),
 			),
 		}
-	
+
 		for menu in allMenues:
 			for subMenu in allMenues[menu]:
 				action = QAction(subMenu[0], self)
 				action.setShortcut(subMenu[1])
 				action.triggered.connect(subMenu[2])
 				menu.addAction(action)
-		
-		self.setGeometry(300, 300, 300, 200)
+
+		# self.setGeometry(300, 300, 300, 200)
+		self.showMaximized()
+		# self.setFocus()
+		# self.setWindowState()
 
 	def addCard(self):
 		pass
@@ -145,13 +393,31 @@ class MainWindow(QMainWindow):
 		pass
 	def generatePool(self):
 		pass
-		
+
 def run():
+	print('initing')
 	Globals.init()
+	print('init done')
 	app=QtWidgets.QApplication(sys.argv)
 	w=MainWindow()
 	w.show()
 	sys.exit(app.exec_())
-	
+
+def test():
+	print('lego')
+	app=QtWidgets.QApplication(sys.argv)
+	print('okay')
+	# pixmap_1 = ImageLoader.get_default_pixmap().get()
+	pixmap_1 = imageload.Loader.get_default_image().get()
+	print(pixmap_1)
+	pixmap_2 = ImageLoader.get_default_pixmap().get()
+	print(pixmap_2)
+	app.quit()
+	sys.exit(app.exec_())
+	print('xd')
+	sys.exit()
+
+	# run()
+
 if __name__=='__main__':
 	run()
