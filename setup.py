36a39a7ec707145ec797f3179286f3b85d787970
guldfisk <ce.guldfisk@gmail.com>
2018-06-26 22:05:26 +0200

    hm

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	dependency_links = [
 		'https://github.com/guldfisk/mtgorp/tarball/master#egg=mtgorp-1.0',
 		'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
-		'https://github.com/guldfisk/orp/tarball/master#egg=mtgimg-1.0',
+		'https://github.com/guldfisk/mtgimg/tarball/master#egg=mtgimg-1.0',
 	],
 	include_package_data = True,
 	install_requires = [
