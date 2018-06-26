29a755b015dbbbf501782bffd593b95420200f40
guldfisk <ce.guldfisk@gmail.com>
2018-06-26 21:34:28 +0200

    ?

--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@ import os
 
 from setuptools import setup
 
+
+def package_files(directory):
+	paths = []
+	for path, directories, file_names in os.walk(directory):
+		for filename in file_names:
+			paths.append(os.path.join('..', path, filename))
+	return paths
+
+extra_files = package_files('deckeditor')
+
 setup(
 	name = 'deckeditor',
 	version = '1.0',
 	packages = ['deckeditor'],
+	package_data={'': extra_files},
 	dependency_links = [
 		'https://github.com/guldfisk/mtgorp/tarball/master#egg=mtgorp-1.0',
 		'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
 		'https://github.com/guldfisk/orp/tarball/master#egg=mtgimg-1.0',
 	],
-	package_data = {
-		'deckeditor': [os.path.join('resources', '*.*')],
-	},
 	include_package_data = True,
 	install_requires = [
 		'appdirs',
